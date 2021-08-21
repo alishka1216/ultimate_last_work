ed77a9c0e7df7361f5e950a777b501929a4efd5d
alishka1216 <alishka1216@gmail.com>
2021-08-21 17:26:19 +0600

    удалил лишний код в login добавил объявления автора удалил импорты в urls скрыл регистрацию так как она не указана в тз

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from accounts.views import register_view, UserDetailView, UserUpdateView, UserChangePasswordView
+from accounts.views import UserDetailView
 from django.contrib.auth.views import LoginView, LogoutView
 
 
@@ -8,9 +8,6 @@ app_name = 'accounts'
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login',),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('register/', register_view, name='register'),
     path('profile/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
     # path('list/', UserListView.as_view(), name='user-list'),
-    path('profile/', UserUpdateView.as_view(), name='user-update'),
-    path('change_password/', UserChangePasswordView.as_view(), name='user-change-password'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -11,18 +11,18 @@ from django.contrib.auth import update_session_auth_hash
 # Create your views here.
 
 
-def register_view(request, *args, **kwargs):
-    context = {}
-    form = UserRegisterForm()
-    if request.method == 'POST':
-        form = UserRegisterForm(data=request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            form.save()
-            # return redirect('album-list')
-    context['form'] = form
-    return render(request, 'registration/register.html', context=context)
+# def register_view(request, *args, **kwargs):
+#     context = {}
+#     form = UserRegisterForm()
+#     if request.method == 'POST':
+#         form = UserRegisterForm(data=request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             login(request, user)
+#             form.save()
+#             # return redirect('album-list')
+#     context['form'] = form
+#     return render(request, 'registration/register.html', context=context)
 
 
 class UserDetailView(DetailView):
@@ -32,4 +32,20 @@ class UserDetailView(DetailView):
     paginate_related_by = 5
     paginate_related_orphans = 0
 
+    # def get_context_data(self, **kwargs):
+    #     albums = self.get_object().albums.all()
+    #     paginator = Paginator(albums, self.paginate_related_by, orphans=self.paginate_related_orphans)
+    #     page_number = self.request.GET.get('page', 1)
+    #     page = paginator.get_page(page_number)
+    #     kwargs['page_obj'] = page
+    #     kwargs['albums'] = page.object_list
+    #     kwargs['is_paginated'] = page.has_other_pages()
+    #     return super().get_context_data(**kwargs)
+
+
+# class UserListView(PermissionRequiredMixin, ListView):
+#     template_name = 'user_list.html'
+#     model = Profile
+#     context_object_name = 'profiles'
+#     permission_required = 'accounts.view_profile'
 
